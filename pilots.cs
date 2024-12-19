f3ea0f12c6079f1e5d37aa7f6151c224e55b1ec7
Avokado1024 <144896565+Avokado1024@users.noreply.github.com>
2024-12-19 19:55:44 +0300

    Create pilots.cs

---
+++ pilots.cs
@@ -0,0 +1,9 @@
+namespace laboratory_12.Models
+{
+    public class Pilots
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
